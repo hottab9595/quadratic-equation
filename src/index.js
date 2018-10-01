2126b7b7dbcb5540760c87cebff0572646601462
Egor Koshel <hottab9595@gmail.com>
2018-10-01 17:29:32 +0300

    quadratic-equation_fix: impliment function quadratic-equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var findNum = /[+-]?\s?\d+/g;
+  var numbers = equation.match(findNum);
+  numbers.splice(1, 1);
+  for (var i = 1; i < 3; i++) {
+      console.log(numbers[i]);
+      numbers[i] = numbers[i].replace(" ", "");
+      numbers[i] = numbers[i].replace("+", "");
+  }
+  var a = Number(numbers[0]);
+  var b = Number(numbers[1]);
+  var c = Number(numbers[2]);
+
+  var D = b*b - 4*a*c;
+  var x1;
+  var x2;
+  if (/-/.test(numbers[0])) {
+      x1 = (-b + Math.round(Math.sqrt(D))) / (2 * a);
+      x2 = (-b - Math.round(Math.sqrt(D))) / (2 * a);
+  } else {
+      x1 = (-b - Math.round(Math.sqrt(D))) / (2 * a);
+      x2 = (-b + Math.round(Math.sqrt(D))) / (2 * a);
+  }
+  const arr = [];
+  arr.push(x1);
+  arr.push(x2);
+return arr;
 }
